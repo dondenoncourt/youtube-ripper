afd9255225e440a29ff282c3da511376dfa9d16e
dondenoncourt <dondenoncourt@gmail.com>
2014-08-20 09:11:46 -0400

    aws list env before deploy

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,3 +1,5 @@
+Chef::Log.info("Running deploy/before_restart.rb")
+
 # load youtube-dl
 `curl https://yt-dl.org/downloads/2014.08.10/youtube-dl -o /usr/bin/youtube-dl`
 `chmod a+x /usr/bin/youtube-dl`
@@ -5,3 +7,11 @@
 `rm -Rf /usr/local/lib/ruby/gems/2.1.0/gems/rack-1.6.0.beta/`
 # this installs 1.5.2
 `gem install rack`
+
+# map the environment_variables node to ENV
+node[:deploy].each do |application, deploy|
+  deploy[:environment_variables].each do |key, value|
+    Chef::Log.info("Setting ENV[#{key}] to #{value}")
+    ENV[key] = value
+  end
+end
